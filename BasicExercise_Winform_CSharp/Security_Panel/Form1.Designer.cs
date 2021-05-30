66460c8ad122c371995e9adab0f5ebbe83f530cf
tungchu1905 <chutrieutung2000@gmail.com>
2021-05-30 22:09:02 +0700

    exercise

---
+++ BasicExercise_Winform_CSharp/Security_Panel/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace Security_Panel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxCal = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBoxCal
+            // 
+            this.textBoxCal.Enabled = false;
+            this.textBoxCal.Location = new System.Drawing.Point(158, 40);
+            this.textBoxCal.Name = "textBoxCal";
+            this.textBoxCal.Size = new System.Drawing.Size(214, 20);
+            this.textBoxCal.TabIndex = 0;
+            this.textBoxCal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(213, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(268, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(49, 39);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(323, 91);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(49, 39);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.Text = "C";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(323, 147);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(49, 39);
+            this.buttonEnter.TabIndex = 8;
+            this.buttonEnter.Text = "Enter";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(268, 147);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(49, 39);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(213, 147);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(49, 39);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(158, 147);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 39);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(323, 205);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(49, 39);
+            this.button0.TabIndex = 12;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.Button0_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(268, 205);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(49, 39);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(213, 205);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(49, 39);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(158, 205);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(49, 39);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 18);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(28, 276);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(340, 95);
+            this.listBox1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 383);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxCal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxCal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ BasicExercise_Winform_CSharp/Security_Panel/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Security_Panel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxCal.TextLength < 4)
+                textBoxCal.Text += button1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBoxCal.TextLength < 4)
+                textBoxCal.Text += button2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBoxCal.TextLength < 4)
+                textBoxCal.Text += button3.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBoxCal.TextLength < 4)
+                textBoxCal.Text += button4.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBoxCal.TextLength < 4)
+                textBoxCal.Text += button5.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBoxCal.TextLength < 4)
+                textBoxCal.Text += button6.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBoxCal.TextLength < 4)
+                textBoxCal.Text += button7.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBoxCal.TextLength < 4)
+                textBoxCal.Text += button8.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBoxCal.TextLength < 4)
+                textBoxCal.Text += button9.Text;
+        }
+
+        private void Button0_Click(object sender, EventArgs e)
+        {
+            if (textBoxCal.TextLength < 4)
+                textBoxCal.Text += button0.Text;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxCal.Text = "";
+        }
+        string[] pass = {"6990","6999","0100","9998","1006","1007","1008" };
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            bool check = false;
+            DateTime now = DateTime.Now;
+            for (int i = 0; i < pass.Length; i++)
+            {
+                if (textBoxCal.Text.Equals(pass[i]))
+                {
+                    check = true;
+                    break;
+                }
+            }
+            if(check == true)
+            {
+                listBox1.Items.Add(now + " Access Success!");
+            }
+            else
+            {
+                listBox1.Items.Add(now + " Access Denie!");
+            }
+            
+
+        }
+    }
+}
